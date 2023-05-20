bd5f34bd7447ecd00ab57c44f4b12bba336c93fc
Ralph Lawrence <ralph@zerojargon.com>
2023-05-20 18:01:37 +0100

    PL-3 - Disable eslint issue

--- src/components/organisms/productFilters.tsx
+++ src/components/organisms/productFilters.tsx
@@ -14,7 +14,7 @@ const ProductFilters: React.FC = () => {
         const updatedQueryParams = { ...queryParams, search: searchQuery };
         const query = qs.stringify(updatedQueryParams);
         navigate(`?${query}`);
-    }, [searchQuery]);
+    }, [searchQuery]); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <Card>
