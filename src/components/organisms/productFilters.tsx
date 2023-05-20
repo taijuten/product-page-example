3b81bfd4cddb8177603a88011a7fe66db0d3dae6
Ralph Lawrence <ralph@zerojargon.com>
2023-05-20 17:59:33 +0100

    Revert "Revert "PL-3 - Add pagination""
    This reverts commit 8378c139cdfa26a94d453288de4a215572d432cd.

--- src/components/organisms/productFilters.tsx
+++ src/components/organisms/productFilters.tsx
@@ -14,7 +14,7 @@ const ProductFilters: React.FC = () => {
         const updatedQueryParams = { ...queryParams, search: searchQuery };
         const query = qs.stringify(updatedQueryParams);
         navigate(`?${query}`);
-    }, [searchQuery, navigate, queryParams]);
+    }, [searchQuery]);
 
     return (
         <Card>
